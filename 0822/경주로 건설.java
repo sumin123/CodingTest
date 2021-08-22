d68681ba05f61c2a6bf0045589ca39e49dd7ef2b
seyeong <seyoung7555@naver.com>
2021-08-22 23:18:54 +0900

    Fix conflict

--- 0822/경주로 건설.java
+++ 0822/경주로 건설.java
@@ -1,14 +1,3 @@
-<<<<<<< HEAD
-class Solution {
-    public int solution(int[][] board) {
-        int answer = 0;
-
-        Queue<Node> q = new LinkedList<>();
-
-        return answer;
-    }
-}
-=======
 import java.util.*;
 class Solution {
 
